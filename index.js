7309014691369ca6d1de1ca875d92a2418fdf362
BHAVESH GAVALI <bgavali@BHAVESHs-MacBook-Pro.local>
2024-11-20 20:59:36 -0500

    updated cors permissions

--- index.js
+++ index.js
@@ -5,27 +5,43 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 require("dotenv").config();
-app.use(cors());
 
-const WS_ALLOWED_ORIGINS = process.env.WS_ALLOWED_ORIGINS.split(",");
-const PORT = process.env.PORT;
+app.use(
+  cors({
+    origin: [
+      "https://call-event-notifier.vercel.app",
+      "https://call-event-display.vercel.app",
+    ], // Allow specific origins
+    methods: ["GET", "POST"],
+  })
+);
+
+const PORT = process.env.PORT || 4101;
 
 const server = http.createServer(app);
-const io = new Server(server);
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "https://call-event-notifier.vercel.app",
+      "https://call-event-display.vercel.app",
+    ], // Allow only these origins
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
-  console.log("USER CONNECTED BUT NOT JOINED ANY ROOM YET ");
+  console.log("USER CONNECTED BUT NOT JOINED ANY ROOM YET");
 
   socket.on("join", (data) => {
     socket.join(data);
     socket.emit("wsConnected", "true");
-    console.log("USER CONNECTED IN ROOM ", data);
+    console.log("USER CONNECTED IN ROOM", data);
   });
 
   socket.on("requestReceived", (data) => {
-    console.log("Request Received--->", data);
-
-    socket.to(data.room).emit("requestResponse", data.request);
+    console.log("Request Received --->", data);
+    socket.to(data.room).emit("requestResponse", data.request); // Emit to room
   });
 
   socket.on("disconnect", () => {
@@ -33,6 +49,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT || 4101, () => {
-  console.log(`Server Started`);
+server.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
